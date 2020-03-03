de8702229cc5c2eaf4812a807b38d86a5019bf60
Tazodiac <tasmyn.clark@gmail.com>
2020-03-03 10:40:07 -0500

    Add best practices

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var enterLetter = prompt('Enter a Letter');
 
 for (i = 0; i < 10; i++) {
-  for (l = 0; l < i+1; l++) {
+  for (l = 0; l < i + 1; l++) {
     document.write(enterLetter);
   }
   document.write('<br>');
